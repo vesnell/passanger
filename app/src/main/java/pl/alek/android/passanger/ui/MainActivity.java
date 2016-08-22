714dbb9af938665a06582692bc646f6bdd896d64
alek <alek666@gmail.com>
2016-08-22 12:19:37 +0200

    input station name from phone

--- app/src/main/java/pl/alek/android/passanger/ui/MainActivity.java
+++ app/src/main/java/pl/alek/android/passanger/ui/MainActivity.java
@@ -2,10 +2,18 @@ package pl.alek.android.passanger.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import pl.alek.android.passanger.R;
 import pl.alek.android.passanger.model.Station;
 import pl.alek.android.passanger.online.service.HttpCallback;
@@ -17,14 +25,45 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity implements Callback<List<Station>> {
 
     private static final String TAG = "MainActivity";
-    private static final String STATION = "Brwinów";
+    private static final int START_SEARCH_SIZE_TEXT = 3;
+
+    @Bind(R.id.etStationSearch)
+    EditText etStationSearch;
+    @Bind(R.id.btnStationSearch)
+    Button btnStationSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        sendRequest(STATION);
+        btnStationSearch.setEnabled(false);
+        etStationSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable editable) {
+                enableSubmitIfReady();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+    }
+
+    private void enableSubmitIfReady() {
+        boolean isReady = etStationSearch.getText().toString().length() >= START_SEARCH_SIZE_TEXT;
+        btnStationSearch.setEnabled(isReady);
+    }
+
+    @OnClick(R.id.btnStationSearch)
+    public void submit() {
+        String station = etStationSearch.getText().toString();
+        sendRequest(station);
     }
 
     private void sendRequest(String stationName) {
@@ -37,9 +76,13 @@ public class MainActivity extends AppCompatActivity implements Callback<List<Sta
     public void onResponse(Call<List<Station>> call, Response<List<Station>> response) {
         if (response.code() == 200) {
             List<Station> stations = response.body();
-            if (stations.size() > 0) {
+            if (stations.size() == 1) {
                 Station station = stations.get(0);
-                Log.d(TAG, station.ID + "; " + station.Nazwa);
+                //open StationInfoActivity
+            } else if (stations.size() > 0) {
+                //open ListStationsActivity
+            } else {
+                Toast.makeText(this, R.string.station_not_found, Toast.LENGTH_LONG).show();
             }
         }
     }
