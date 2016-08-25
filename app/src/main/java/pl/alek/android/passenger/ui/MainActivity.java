8b587b5f27aa9aee096e672436bbae18f7ba8323
alek <alek666@gmail.com>
2016-08-25 13:35:53 +0200

    add custom icon, splash screen

--- app/src/main/java/pl/alek/android/passenger/ui/MainActivity.java
+++ app/src/main/java/pl/alek/android/passenger/ui/MainActivity.java
@@ -18,15 +18,12 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.crashlytics.android.Crashlytics;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import io.fabric.sdk.android.Fabric;
 import pl.alek.android.passenger.R;
 import pl.alek.android.passenger.model.Station;
 import pl.alek.android.passenger.online.service.HttpCallback;
@@ -54,7 +51,6 @@ public class MainActivity extends AppCompatActivity implements Callback<ArrayLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
---
+++ app/src/main/java/pl/alek/android/passenger/ui/SplashActivity.java
@@ -0,0 +1,24 @@
+package pl.alek.android.passenger.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
+/**
+ * Created by Lenovo on 25.08.2016.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
