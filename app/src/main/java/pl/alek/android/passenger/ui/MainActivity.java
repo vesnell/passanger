29ccc179f38c2dc26ada728d4f0c313dee8e707a
alek <alek666@gmail.com>
2016-09-23 12:33:56 +0200

    add proguard

--- app/src/main/java/pl/alek/android/passenger/ui/MainActivity.java
+++ app/src/main/java/pl/alek/android/passenger/ui/MainActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import butterknife.Bind;
